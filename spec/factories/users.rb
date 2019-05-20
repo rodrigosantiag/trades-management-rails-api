21e12f512934e4ccc36d4b2a30ecdeecd0956f7c
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-05-20 00:07:13 -0300

    Adding initial tests to User Model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    email {Faker::Internet.email}
+    name {Faker::Name.name}
+    password {'12345678'}
+    password_confirmation {'12345678'}
+    risk {7}
   end
 end
