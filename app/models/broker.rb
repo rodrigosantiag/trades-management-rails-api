2a55809631807bd782e6cc2df1fbc62a6963d2a6
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-05-21 05:19:15 -0300

    Adding association between users and brokers ans tests associations

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -1,5 +1,5 @@
 class Broker < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :name
+  validates_presence_of :name, :user
 end
--- spec/models/broker_spec.rb
+++ spec/models/broker_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Broker, type: :model do
-  let(:task) {build(:task)}
+  let(:broker) {build(:broker)}
 
   it {is_expected.to belong_to(:user)}
 
-  context 'when is new' do
-    it {expect(:name).not_to be_nil}
-  end
-
   it {is_expected.to validate_presence_of(:name)}
+  it {is_expected.to validate_presence_of(:user)}
   it {is_expected.to respond_to(:name)}
-  it {is_expected.to respond_to(:user_id)}
+  it {is_expected.to respond_to(:user)}
 end
