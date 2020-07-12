68e4208e9ee02393d09c3dba3cf2a2618ace7ae7
Rodrigo Santiago <rodrigosantiag@gmail.com>
2020-07-12 11:57:07 -0300

    Updateing devise and devise_token_auth to remove DEPRECATE WARNING Implementing methods create, show and update for strategies_controller.rb and respective functional tests

--- app/controllers/api/v1/strategies_controller.rb
+++ app/controllers/api/v1/strategies_controller.rb
@@ -8,4 +8,37 @@ class Api::V1::StrategiesController < ApplicationController
 
     render json: strategies, status: 200
   end
+
+  def show
+    strategy = current_user.strategies.find params[:id]
+
+    render json: strategy, status: 200
+  end
+
+  def create
+    strategy = current_user.strategies.build(strategy_params)
+
+    if strategy.save
+      render json: strategy, status: 201
+    else
+      render json: {errors: strategy.errors}, status: 422
+    end
+  end
+
+  def update
+    strategy = current_user.strategies.find(params[:id])
+
+    if strategy.update(strategy_params)
+      render json: strategy, status: 200
+    else
+      render json: {errors: strategy.errors}, status: 422
+    end
+  end
+
+  # TODO: delete methos and tests
+
+  private
+  def strategy_params
+    params.require(:strategy).permit(:name)
+  end
 end
--- spec/requests/api/v1/strategies_spec.rb
+++ spec/requests/api/v1/strategies_spec.rb
@@ -29,7 +29,118 @@ RSpec.describe 'Strategy API', type: :request do
         expect(json_body[:data].count).to eq(10)
       end
     end
+
+    context 'Return list of strategies in alphabetical order' do
+      let!(:strategy1) { create(:strategy, name: 'Strategy B', user_id: user.id) }
+      let!(:strategy2) { create(:strategy, name: 'Strategy A', user_id: user.id) }
+      let!(:strategy3) { create(:strategy, name: 'Strategy C', user_id: user.id) }
+
+      before do
+        get '/strategies', params: {}, headers: headers
+      end
+
+      it 'should return ordered by name' do
+        returned_strategies = json_body[:data].map {|s| s[:attributes][:name]}
+
+        expect(returned_strategies).to eq([strategy2.name, strategy1.name, strategy3.name])
+      end
+    end
   end
 
-#  TODO: implement test for alphabetical order strategy
+  describe 'GET /strategies/:id' do
+    let(:strategy) {create(:strategy, user_id: user.id)}
+
+    before {get "/strategies/#{strategy.id}", params: {}, headers: headers}
+
+    it 'should return status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should return strategy data' do
+      expect(json_body[:data][:attributes][:name]).to eq(strategy.name)
+    end
+  end
+
+  describe 'POST /strategies' do
+    before do
+      post '/strategies', params: {strategy: strategy_params}.to_json, headers: headers
+    end
+
+    context 'when params are valid' do
+      let(:strategy_params) {attributes_for(:strategy)}
+
+      it 'should return status code 201' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'should save the strategy in database' do
+        expect(Strategy.find_by_name(strategy_params[:name])).not_to be_nil
+      end
+
+      it 'should return strategy data' do
+        expect(json_body[:data][:attributes][:name]).to eq(strategy_params[:name])
+      end
+
+      it 'should assign stratey to user' do
+        expect(json_body[:data][:attributes][:'user-id']).to eq(user.id)
+      end
+    end
+
+    context 'when params are invalid' do
+      let(:strategy_params) {attributes_for(:strategy, name: ' ')}
+
+      it 'should return status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'should not save strategy in database' do
+        expect(Strategy.find_by_name(strategy_params[:name])).to be_nil
+      end
+
+      it 'should return error key for name' do
+        expect(json_body[:errors]).to have_key(:name)
+      end
+    end
+  end
+
+  describe 'PUT /strategies/:id' do
+    let!(:strategy) {create(:strategy, user_id: user.id)}
+
+    before do
+      put "/strategies/#{strategy.id}", params: {strategy: strategy_params}.to_json, headers: headers
+    end
+
+    context 'when params are valid' do
+      let(:strategy_params) {{name: 'New strategy name'}}
+
+      it 'should return status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'should save updated strategy in database' do
+        expect(Strategy.find_by_name(strategy_params[:name])).not_to be_nil
+      end
+
+      it 'should return updated strategy data' do
+        expect(json_body[:data][:attributes][:name]).to eq(strategy_params[:name])
+      end
+    end
+
+    context 'when parameters are not valid' do
+      let(:strategy_params) {{name: ' '}}
+
+      it 'should return status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'should not save invalid strategy in database' do
+        expect(Strategy.find_by_name(strategy_params[:name])).to be_nil
+      end
+
+      it 'should have key :name in errors' do
+        expect(json_body[:errors]).to have_key(:name)
+      end
+    end
+  end
+
 end
