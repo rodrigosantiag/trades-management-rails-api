36c838089adc02958286f2dd1982f00b37dc18f7
Rodrigo Santiago <rodrigosantiag@gmail.com>
2020-07-25 14:38:31 -0300

    Informing Ruby version in Gemfile Removing 'api' subdomain from routes.rb Configuring auto deploy to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {confirmations: 'confirmations', passwords: 'passwords'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: {format: :json}, path: '/', constraints: {subdomain: 'api'} do
+  namespace :api, defaults: {format: :json}, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       mount_devise_token_auth_for 'User', at: 'auth'
       resources :brokers, only: [:index, :show, :create, :update, :destroy]
