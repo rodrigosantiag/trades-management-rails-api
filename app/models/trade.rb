b38f21c0c71074596df3d86ae2444dffe09adfbe
Rodrigo Santiago <rodrigosantiag@gmail.com>
2020-08-02 11:26:35 -0300

    Refactoring trade.rb and trade_serializer.rb

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -6,29 +6,31 @@ class Trade < ApplicationRecord
   belongs_to :user
   belongs_to :strategy, optional: true
 
-  after_validation :set_result_balance, on: [:create, :update]
-  after_commit :update_account_balance, on: [:create, :update, :destroy]
+  after_validation :set_result_balance, on: %i[create update]
+  after_commit :update_account_balance, on: %i[create update destroy]
 
   def set_result_balance
-    if self.result?
-      self.result_balance = self.value * self.profit / 100
-    else
-      self.result_balance = -self.value
-    end if self.errors.empty?
+    return unless errors.empty?
+
+    self.result_balance = if result?
+                            value * profit / 100
+                          else
+                            -value
+                          end
+
   end
 
-=begin
-  TODO: think better about updating account balance after an account deposit or refill.
-        Ideas: should I update current_balance or initial_balance?
-        Probably it'd be necessary to create a flag to reset trades summing by ID (create a column on accounts table *trade_count_id* and below this id I'd have to sum to get right statistics)
-=end
+  #   TODO: think better about updating account balance after an account deposit or refill.
+  #         Ideas: should I update current_balance or initial_balance?
+  #         Probably it'd be necessary to create a flag to reset trades summing by ID (create a column on accounts table
+  #         *trade_count_id* and below this id I'd have to sum to get right statistics)
 
 
   def update_account_balance
-    trade_account = Account.find_by_id(self.account_id)
-    if trade_account
-      current_balance = trade_account.initial_balance + trade_account.trades.sum(:result_balance)
-      trade_account.update(:current_balance => current_balance)
-    end
+    trade_account = Account.find_by_id(account_id)
+    return unless trade_account
+
+    current_balance = trade_account.initial_balance + trade_account.trades.sum(:result_balance)
+    trade_account.update(current_balance: current_balance)
   end
 end
