28f8110f4500451e0626c59b18fbfc452cb97492
Rodrigo Santiago <rodrigosantiag@gmail.com>
2022-12-20 08:55:50 -0300

    Adjust mail test send

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,10 +42,6 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
-  # Mailcatcher
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
   # Print deprecation notices to the stderr.
