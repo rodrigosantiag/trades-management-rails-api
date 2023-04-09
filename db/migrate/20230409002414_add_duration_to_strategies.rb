2be671ca555d661e0a2cc211f56f662bd60acfa2
Rodrigo Santiago <rodrigosantiag@gmail.com>
2023-04-08 22:38:33 -0300

    Add migration to add duration time in minutes to strategy

---
+++ db/migrate/20230409002414_add_duration_to_strategies.rb
@@ -0,0 +1,9 @@
+class AddDurationToStrategies < ActiveRecord::Migration[7.0]
+  def up
+    add_column :strategies, :duration, :integer
+  end
+
+  def down
+    remove_column :strategies, :duration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_18_131333) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_09_002414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_18_131333) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "duration"
     t.index ["user_id"], name: "index_strategies_on_user_id"
   end
 
